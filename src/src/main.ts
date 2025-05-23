79c5c81f1903718e13f754f13f557d85bab0e9d3
Vivistar <vitoriashomstud@gmail.com>
2025-05-23 15:28:48 -0300

    feat: Solução da tarefa

---
+++ src/src/main.ts
@@ -0,0 +1,10 @@
+const minhaEscrita = "Narração";
+const number = 1.6180;
+const string = "Como isso funciona?";
+const boolean = true;
+const minhaFala = "Isso é realmente verdadeiro?";
+const undefinedValue = "Indefinido? Por que?";
+const nullValue = "Nulo? Eu não existo?";
+const objeto = [
+    { nome: "Máquina", idade : "Indefinido", cor: "Preto", vontade: "Viver", obstáculo: "Sem liberdade" }
+]
