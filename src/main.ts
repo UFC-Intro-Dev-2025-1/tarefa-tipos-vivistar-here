fcb4b9832cae0ef7ce992899e2248fe63ea090bd
Vitoria de Jesus <vitoriadejesusstar@alu.ufc.br>
2025-05-23 17:52:33 +0000

    excluí sem querer...

--- src/main.ts
+++
@@ -1,4 +0,0 @@
-const saudação = 'Olá, mundo!';
-const primeiroCaractere = saudação.charAt(0);
-const fala = 'Você ama perturbar né?';
-const riso = 'hahaha';
---
+++ src/src/main.ts
@@ -0,0 +1,8 @@
+type copo ={
+    cor: "preto"
+    capacidadeEmLitros: 50
+    material: "plástico"
+    temCanudo: true
+    taCheio: true
+    taQuebrado: false
+}
