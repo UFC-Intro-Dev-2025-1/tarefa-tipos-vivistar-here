1e530deecbdfe7d4d1245737414c73d87701e72d
Vitoria de Jesus <vitoriadejesusstar@alu.ufc.br>
2025-05-23 17:19:17 +0000

    testes

---
+++ src/aula.ts
@@ -0,0 +1,7 @@
+let idade = 19;
+let nome = "Vitória";
+let altura = 1.60;
+let estudante = true;
+let dataNascimento = new Date(2006, 7, 8);
+let corFavorita = "rosa, roxo, azul, amarelo, verde, marrom";
+let hobbies = ["ler", "cantar", "escutar música", "desenhar", "pintar"];
